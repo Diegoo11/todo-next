a32b8521ae380dd8019b918344c9e17e40584a99
Diegoo11 <ynoacamino@gmail.com>
2023-08-12 15:08:33 -0500

    format to init project

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,23 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        downy: {
+          DEFAULT: '#74D3BB',
+          50: '#FFFFFF',
+          100: '#F0FAF8',
+          200: '#D1F0E8',
+          300: '#B2E7D9',
+          400: '#93DDCA',
+          500: '#74D3BB',
+          600: '#49C6A6',
+          700: '#34A387',
+          800: '#267964',
+          900: '#194E40',
+          950: '#12392F',
+        },
+      },
     },
   },
   plugins: [],
-}
+};
