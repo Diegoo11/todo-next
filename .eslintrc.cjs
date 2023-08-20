4f633b921edb3a7f7359ff3644e85b1d5f462ac0
Diegoo11 <ynoacamino@gmail.com>
2023-08-19 22:00:59 -0500

    config to linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,5 +8,9 @@ module.exports = {
   rules: {
     'linebreak-style': 0,
     'import/prefer-default-export': 0,
+    'import/no-unresolved': 0,
+    'import/extensions': 0,
+    camelcase: 0,
+    'jsx-a11y/label-has-associated-control': 0,
   },
 };
