1f9f57b6d93a83a4f5e79e2efd57eb0c69f33606
Diegoo11 <ynoacamino@gmail.com>
2023-08-19 22:02:03 -0500

    add config middleware to redirect

---
+++ middleware.js
@@ -0,0 +1,8 @@
+// This function can be marked `async` if using `await` inside
+
+export { default } from 'next-auth/middleware';
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ['/logout', '/addTask', '/editTask', '/profile'],
+};
